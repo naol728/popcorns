50d77ee3976cb7f50cb31e1948a4a0c84d069465
naol <naolmeseret22@gmail.com>
2024-08-01 14:24:15 +0300

    i changed the reanderd componet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-// import './App.css';
-// import App from './App';
-import Starrating from "./Step";
+import App from './App';
+// import Starrating from "./Step";
 
  const root=ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     {/* <App /> */}
-     <Starrating  maxrating={10}/>
+     <App />
+     {/* <Starrating  maxrating={10}/> */}
   </React.StrictMode>
 )
