413099425c036923c0a0a4ffb2c45e93ff8e7a28
naol <naolmeseret22@gmail.com>
2024-08-01 14:15:31 +0300

    componates are done and some styleing are add

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+// import './App.css';
+// import App from './App';
+import Starrating from "./Step";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ const root=ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+     {/* <App /> */}
+     <Starrating  maxrating={10}/>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
