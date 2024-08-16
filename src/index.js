186e8cb4111484ff601b55cd7539972a28ac8a9c
naol <naolmeseret22@gmail.com>
2024-08-16 20:56:20 +0300

    i started version 3 of my aplication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import App from './App-v2';
+import App from './App-v3';
 // import Starrating from "./Step";
 
 const root=ReactDom.createRoot(document.getElementById("root"));
